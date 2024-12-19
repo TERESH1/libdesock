64e1e5d839e9ec3948e9489fc5afdda2f97ec909
TERESH1 <svyatoslavtereshin@yandex.ru>
2024-12-19 08:59:37 +0000

    Replace reading from stdin to settable buffer

--- src/hooks.c
+++ src/hooks.c
@@ -8,15 +8,34 @@
 
 #include <unistd.h>
 
+#include "util.h"
 #include "hooks.h"
 #include "syscall.h"
- 
+
+static struct {
+    const unsigned char *buffer;
+    size_t len;
+    size_t offset;
+} input_buffer;
+
+VISIBLE
+void libdesock_hooks_buffer_init(const unsigned char *buf, size_t len) {
+    input_buffer.buffer = buf;
+    input_buffer.len = len;
+    input_buffer.offset = 0;
+}
+
 /*  This function is called whenever a read on a network
  *  connection occurs. It MUST return the number of bytes
  *  written to buf or -1 if an error occurs.
  */
 ssize_t hook_input (char* buf, size_t size) {
-    return syscall_cp(SYS_read, 0, buf, size);
+    ssize_t available_len = input_buffer.len - (ssize_t) input_buffer.offset;
+    if (UNLIKELY(available_len < 0)) return -1;
+    size_t result_len = (size_t) available_len < size? available_len : size;
+    __builtin_memcpy(buf, input_buffer.buffer + input_buffer.offset, result_len);
+    input_buffer.offset += result_len;
+    return result_len;
 }
 
 /*  This function is called whenever a write on a network
@@ -41,5 +60,9 @@ ssize_t hook_output (const char* buf, size_t size) {
  *  stream position.
  */
 off_t hook_seek (off_t offset) {
-    return lseek(0, offset, SEEK_CUR);
+    off_t new_offset = input_buffer.offset + offset;
+    if (UNLIKELY(new_offset < 0 || (size_t) new_offset > input_buffer.len))
+        return -1;
+    input_buffer.offset = new_offset;
+    return new_offset;
 }
